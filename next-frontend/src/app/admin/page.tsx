5ef9128ececa977297fb54b87b53da79c77c6379
lukas10067 <lukas.oliveira@agencian1.com.br>
2024-12-28 14:07:46 -0300

    resolvendo conflitos

--- next-frontend/src/app/admin/page.tsx
+++ next-frontend/src/app/admin/page.tsx
@@ -1,11 +1,58 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useMap } from "../../hooks/useMap";
+import { socket } from "../../utils/socket-io";
 
 export function AdminPage() {
   const mapContainerRef = useRef<HTMLDivElement>(null);
-  useMap(mapContainerRef);
+  const map = useMap(mapContainerRef);
+
+  useEffect(() => {
+    if (!map) {
+      return;
+    }
+    if (socket.connected) {
+      console.log('Socket conectado com sucesso!');
+    } else {
+      console.log('Socket conectando...');
+      socket.connect();
+      socket.once('connect', () => {
+        console.log('Socket conectado com sucesso!!');
+      });
+    }
+
+    socket.on('disconnect', () => {
+      console.log('Socket desconectado.');
+    });
+
+    socket.on(
+      `server:new-points:list`,
+      async (data: { route_id: string; lat: number; lng: number }) => {
+        console.log(data);
+        if (!map.hasRoute(data.route_id)) {
+          const response = await fetch(`http://localhost:3001/api/routes/${data.route_id}`)
+          const route = await response.json();
+          map.addRouteWithIcons({
+            routeId: data.route_id,
+            startMarkerOptions: {
+              position: route.directions.routes[0].legs[0].start_location
+            },
+            endMarkerOptions: {
+              position: route.directions.routes[0].legs[0].end_location
+            },
+            carMarkerOptions: {
+              position: route.directions.routes[0].legs[0].start_location
+            },
+          });
+        }
+        map.moveCar(data.route_id, { lat: data.lat, lng: data.lng });
+      }
+    );
+    return () => {
+      socket.disconnect();
+    }
+  }, [map]);
 
   return <div className="h-full w-full" ref={mapContainerRef} />;
 }
